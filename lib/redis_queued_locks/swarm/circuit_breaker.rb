54b0d478541155c2a72453963cbd895ae5bca041
Rustam Ibragimov <iamdaiver@gmail.com>
2024-08-04 11:57:48 +0300

    [swarm/circuit-breaker] let's go

---
+++ lib/redis_queued_locks/swarm/circuit_breaker.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# @api private
+# @since 0.20.0
+class RedisQueuedLocks::Swarm::CircuitBreaker
+  # @return [void]
+  #
+  # @api private
+  # @since 0.20.0
+  def initialize
+  end
+end
